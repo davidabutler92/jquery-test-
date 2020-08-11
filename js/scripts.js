1bf160d8b26a356bdbaaa81168e1c847d5a60117
davidabutler92 <davidabutler92@yahoo.com>
2020-08-11 09:54:50 -0700

    add walrus img fade in/out

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,14 @@
 $(document).ready(function() {
-  $("button#black").click(function() {
-    $("body").toggleClass("black-background");
-  });
   $(".clickable").click(function() {
-    $("#walrus-showing").slideToggle();
-    $("#walrus-hidden").slideToggle();
+    $("#walrus-showing").fadeToggle();
+    $("#walrus-hidden").fadeToggle();
+
   });
-  $("#infoToggleWalrus").click(function() {
-    $("#walrusInfo").fadeIn();  
-  });
-  
   $(".click").click(function() {
     $("#whale-showing").fadeToggle();
     $("#whale-hidden").fadeToggle();
   });
-  $("#infoToggleWhale").click(function() {
-    $("#whaleInfo").fadeIn();
+  $("#black").click(function() {
+    $(".body").toggle();
   });
 });
-
-
-
